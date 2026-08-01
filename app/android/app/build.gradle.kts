2a3821c1e550eec2e5ae89cd8310340917283939
Jordan <jordan.tayac@gmail.com>
2026-08-01 11:43:47 +0200

    remove firebase

--- app/android/app/build.gradle.kts
+++ app/android/app/build.gradle.kts
@@ -2,8 +2,6 @@ plugins {
     id("com.android.application")
     // The Flutter Gradle Plugin must be applied after the Android and Kotlin Gradle plugins.
     id("dev.flutter.flutter-gradle-plugin")
-    // Add the Google services Gradle plugin
-    id("com.google.gms.google-services")
 }
 
 android {
@@ -44,14 +42,4 @@ kotlin {
 
 flutter {
     source = "../.."
-}
-
-dependencies {
-  // Import the Firebase BoM
-  implementation(platform("com.google.firebase:firebase-bom:34.15.0"))
-
-
-  // TODO: Add the dependencies for Firebase products you want to use
-  // When using the BoM, don't specify versions in Firebase dependencies
-  // https://firebase.google.com/docs/android/setup#available-libraries
 }
